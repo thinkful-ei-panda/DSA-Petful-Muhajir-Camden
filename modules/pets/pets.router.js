b872d3b5611bb8d6dfa07c5759e5ae8241800087
M-Sayer <haji.san@icloud.com>
2020-08-28 11:33:35 -0400

    implemented service functions in pets router

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -8,10 +8,15 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   // Return all pets currently up for adoption.
+  const pets = Pets.get();
+  return res.send(pets)
 })
 
-router.delete('/', json, (req, res) => {
+router.delete('/:pet_type', json, (req, res) => {
   // Remove a pet from adoption.
+  const type = req.params.pet_type;
+  Pets.dequeue(type)
+  return res.status(204).end()
 })
 
 module.exports = router
