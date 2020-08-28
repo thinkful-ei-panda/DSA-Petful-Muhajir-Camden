4db25fcf3c7e25c42735d2e6603757abefaabe3c
M-Sayer <haji.san@icloud.com>
2020-08-28 11:49:50 -0400

    implemented people router and service

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -1,16 +1,20 @@
-const express = require('express')
-const json = require('body-parser').json()
+const express = require('express');
+const json = require('body-parser').json();
 
-const People = require('./people.service')
+const People = require('./people.service');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
   // Return all the people currently in the queue.
+  return res.send(People.get());
 })
 
 router.post('/', json, (req, res) => {
   // Add a new person to the queue.
+  const newPerson = { ...req.body };
+  People.enqueue(newPerson.name)
+  res.status(204).end()
 })
 
 module.exports = router
